1457dc6e71904b4d57900596c47c5a0a3320ddf4
gnehs <jayuiop@gmail.com>
2022-05-09 00:30:10 +0800

    fixed base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sitcon-2022-issue-generator/',
   plugins: [vue()],
   resolve: {
     alias: {
